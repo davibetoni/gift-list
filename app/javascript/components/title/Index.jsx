1b24f6a6a4e0f276cbc4862863860748ac85b2f6
davibetoni <davibetoni@gmail.com>
2022-02-07 20:47:21 -0300

    css do front no mesmo do projeto

--- app/javascript/components/title/Index.jsx
+++ app/javascript/components/title/Index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import "./style.css";
 
 export default () => (
   <div className="primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
-        <h1 className="display-4 home-title">Davi E Jhulia</h1>
-        <h1 className="display-4 home-date">16 | 12 | 2022</h1>
+        <h1 className="display-4 home_title-title">Davi E Jhulia</h1>
+        <h1 className="display-4 home_title-date">16 | 12 | 2022</h1>
         <hr className="my-4" />
       </div>
     </div>
