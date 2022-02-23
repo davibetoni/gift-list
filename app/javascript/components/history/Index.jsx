f7a3203d501af95438f8984a568b86fccb01b08c
davibetoni <davibetoni@gmail.com>
2022-02-23 10:59:17 -0300

    nova parte

---
+++ app/javascript/components/history/Index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => (
+  <div className="primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <p>Teste</p>
+    </div>
+  </div>
+);
