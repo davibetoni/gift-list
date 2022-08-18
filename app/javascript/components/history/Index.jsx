f08b46a0251aba626a099c22e09d0bd4c629ab1a
davibetoni <davibetoni@gmail.com>
2022-08-18 19:12:13 -0300

    upgrading homepage

--- app/javascript/components/history/Index.jsx
+++ app/javascript/components/history/Index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import sp from "./davi-jhu-sp.png";
 
 export default () => (
   <div className="primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
-      <p>Teste</p>
+      <img className="home_sp-image" src={sp} alt="SP" />
     </div>
   </div>
 );
